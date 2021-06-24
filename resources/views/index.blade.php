b790e879c17f007a0d1c8ff3adc9cb5b48a4a124
Farrux <farruxzokirov230596@gmail.com>
2021-06-24 16:41:31 +0500

    Section one

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -22,10 +22,10 @@
       </div>
     </div>
     <div class="section-one-content container mx-auto flex">
-      <div class="first-content">
+      <div class="first-content ">
         <img src="../images/img1.png" alt="">
         <div class="bookmark flex justify-center items-center">
-          <span class="iconify  text-white" data-icon="mdi:bookmark-outline" data-inline="false"></span> 
+          <a href=""><span class="iconify  text-white" data-icon="mdi:bookmark-outline" data-inline="false"></span> </a>
         </div>
                
         <p class="text-white"><span class="iconify text-white" data-icon="ic:sharp-radio-button-checked" data-inline="false"></span> Siyosat</p>
@@ -41,6 +41,83 @@
         </div>
       </div>
       <div class="second-content">
+        <div class="second-content-head flex items-center justify-between">
+          <h1 class="">So’nggi yangiliklar</h1>
+          <a href="">Barchasi</a>
+        </div>
+        <div class="line-gradient"></div>
+        <div class="second-content-body flex">
+          <div class="img-content">
+            <img src="../images/img2.png" alt="">
+          </div>
+          <div class="text-content-second">
+            <div class="icon-text flex items-center justify-between align-middle">
+              <p class="bell flex items-center align-middle"><span class="iconify" data-icon="ic:sharp-radio-button-checked" data-inline="false"></span> Siyosat</p>
+              <div class="date-icons flex items-center">
+                <span class="bell2 flex items-center"><span class="iconify" data-icon="mdi:clock-time-four-outline" data-inline="false"></span> 19:27 / 08.04.2021</span>
+                <span class="bell2 flex items-center"><span class="iconify" data-icon="mdi:message-text" data-inline="false"></span> 28</span>
+              </div>
+            </div>
+            <a class="" href="#">“Yoshlar daftari” ga kirgan va
+              dexqonchilik qilayotgan yoshlarga
+              urug’lik va ko’chat xarid qilish uchun 2
+              million so’mgacha subsidiya beriladi.</a>
+          </div>
+        </div>
+        <div class="line-hr"></div>
+        <div class="second-content-body flex">
+          <div class="img-content">
+            <img src="../images/img3.png" alt="">
+          </div>
+          <div class="text-content-second">
+            <div class="icon-text flex items-center justify-between align-middle">
+              <p class="bell flex items-center align-middle"><span class="iconify" data-icon="ic:sharp-radio-button-checked" data-inline="false"></span> Siyosat</p>
+              <div class="date-icons flex items-center">
+                <span class="bell2 flex items-center"><span class="iconify" data-icon="mdi:clock-time-four-outline" data-inline="false"></span> 19:27 / 08.04.2021</span>
+                <span class="bell2 flex items-center"><span class="iconify" data-icon="mdi:message-text" data-inline="false"></span> 28</span>
+              </div>
+            </div>
+            <a class="" href="#">Birinchi chorakda aholi daromadlari 
+              9,8 foizga oshdi</a>
+          </div>
+        </div>
+        <div class="line-hr"></div>
+        <div class="second-content-body flex">
+          <div class="img-content">
+            <img src="../images/img4.png" alt="">
+          </div>
+          <div class="text-content-second">
+            <div class="icon-text flex items-center justify-between align-middle">
+              <p class="bell flex items-center align-middle"><span class="iconify" data-icon="ic:sharp-radio-button-checked" data-inline="false"></span> Siyosat</p>
+              <div class="date-icons flex items-center">
+                <span class="bell2 flex items-center"><span class="iconify" data-icon="mdi:clock-time-four-outline" data-inline="false"></span> 19:27 / 08.04.2021</span>
+                <span class="bell2 flex items-center"><span class="iconify" data-icon="mdi:message-text" data-inline="false"></span> 28</span>
+              </div>
+            </div>
+            <a class="" href="#">O’zbekiston va Aqsh o’rtasida oliy ta’lim
+              sohasidagi hamkorlikning yangi
+              dinamikasi</a>
+          </div>
+        </div>
+        <div class="line-hr"></div>
+        <div class="second-content-body flex">
+          <div class="img-content">
+            <img src="../images/img5.png" alt="">
+          </div>
+          <div class="text-content-second">
+            <div class="icon-text flex items-center justify-between align-middle">
+              <p class="bell flex items-center align-middle"><span class="iconify" data-icon="ic:sharp-radio-button-checked" data-inline="false"></span> Siyosat</p>
+              <div class="date-icons flex items-center">
+                <span class="bell2 flex items-center"><span class="iconify" data-icon="mdi:clock-time-four-outline" data-inline="false"></span> 19:27 / 08.04.2021</span>
+                <span class="bell2 flex items-center"><span class="iconify" data-icon="mdi:message-text" data-inline="false"></span> 28</span>
+              </div>
+            </div>
+            <a class="" href="#">“Hamma ayb yoshlarda emas. Tan olish
+              kerak, tizim mukammal emas” - 15
+              yoshda talaba bo‘lgan jizzaxlik yigit bilan
+              suhbat</a>
+          </div>
+        </div>
 
       </div>
     </div>

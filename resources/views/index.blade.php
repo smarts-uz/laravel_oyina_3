89238ecdbea5960b56d8bf286baca14b05b55d26
Farrux <farruxzokirov230596@gmail.com>
2021-06-25 08:55:12 +0500

    sections add

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -655,4 +655,109 @@
     </div>
   </section>
 
+  {{-- Section five --}}
+  <section class="section-five container mx-auto">
+    <div class="section-three-first-content-head-text flex items-center justify-between">
+      <h1 class="">Intervyular</h1>
+      <a href="#">Barchasi</a>
+    </div>
+    <div class="line-gradient-five"></div>
+    <div class="interview-cards flex">
+      <a href="" class="interview-cards-content">
+        <div class="interview-img-content">
+          <img src="../images/rew1.png" alt="">
+        </div>
+        <div class="interview-text-content">
+          <p>Yoshlarda kitobxonlik
+            madaniyatini oshirish,
+            kitobga bo’lgan muhabbatni
+            uyg’otish bo’yicha suhbat ...
+            </p>
+          </div>        
+      </a>
+      <a href="" class="interview-cards-content">
+        <div class="interview-img-content">
+          <img src="../images/rew2.png" alt="">
+        </div>
+        <div class="interview-text-content">
+          <p>O’zbekistonda xizmat
+            ko’rsatgan madaniyat xodimi
+            shoira Sharifa Salimova bilan
+            suhbat</p>
+        </div>
+      </a>
+      <a href="" class="interview-cards-content">
+        <div class="interview-img-content">
+          <img src="../images/rew3.png" alt="">
+        </div>
+        <div class="interview-text-content">
+          <p>Xurshid Do’stmuhammad
+            ilm-fan sohasida amalga
+            oshirilayotgan islohotlar
+            to’g’risidagi suhbat</p>
+        </div>
+      </a>
+      <a href="" class="interview-cards-content">
+        <div class="interview-img-content">
+          <img src="../images/rew4.png" alt="">
+        </div>
+        <div class="interview-text-content">
+          <p>O’zbekiston tarixi davlat
+            muzeyi direktori Ismailova
+            Jannat Hamidovna bilan
+            suhbat</p>
+        </div>
+      </a>
+    </div>
+
+  </section>
+
+{{-- section 6 havolalar --}}
+  <section class="section-six container mx-auto">
+    <div class="section-three-first-content-head-text flex items-center justify-between">
+      <h1 class="">Foydali havolalar</h1>
+      <a href="#">Barchasi</a>
+    </div>
+    <div class="line-gradient-five"></div>
+    
+    <div class="grid-template-card">
+      <a href="#" class="grid-card-items">
+        <img src="../images/gerb1.png" alt="">
+        <p>O‘zbekiston Respublikasi Prezidentining rasmiy veb-sayti</p>
+      </a>
+      <a href="#" class="grid-card-items">
+        <img src="../images/gerb1.png" alt="">
+        <p>O‘zbekiston Respublikasi Madaniyat vazirligi</p>
+      </a>
+      <a href="#" class="grid-card-items">
+        <img src="../images/card1.png" alt="">
+        <p>Respublika Ma’naviyat va Ma’rifat markazi</p>
+      </a>
+      <a href="#" class="grid-card-items">
+        <img src="../images/card2.png" alt="">
+        <p>O’zbekiston Badiiy
+          akademiya</p>
+      </a>
+      <a href="#" class="grid-card-items">
+        <img src="../images/card3.png" alt="">
+        <p>O’zbekiston Yozuvchilar
+          uyushmasi</p>
+      </a>
+      <a href="#" class="grid-card-items">
+        <img src="../images/card4.png" alt="">
+        <p>O’zbekiston Islom 
+          sivilizatsiya markazi</p>
+      </a>
+      <a href="#" class="grid-card-items">
+        <img src="../images/card5.png" alt="">
+        <p>O’zbekiston Milliy kutubxonasi</p>
+      </a>
+      <a href="#" class="grid-card-items">
+        <img src="../images/card6.png" alt="">
+        <p>O’zbekiston Fanlar
+          akademiyasi</p>
+      </a>
+    </div>
+  </section>
+
 @endsection

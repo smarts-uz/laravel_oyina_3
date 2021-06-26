121d79a28127d9657c0e34cbd0f44e94baebd5b2
Farrux <farruxzokirov230596@gmail.com>
2021-06-26 22:54:20 +0500

    Change section 6

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -734,38 +734,54 @@
     
     <div class="grid-template-card">
       <a href="#" class="grid-card-items">
-        <img src="../images/gerb1.png" alt="">
+        <div>
+          <img src="../images/gerb1.png" alt="">
+        </div>
         <p>O‘zbekiston Respublikasi Prezidentining rasmiy veb-sayti</p>
       </a>
       <a href="#" class="grid-card-items">
-        <img src="../images/gerb1.png" alt="">
+        <div>
+          <img src="../images/gerb1.png" alt="">
+        </div>
         <p>O‘zbekiston Respublikasi Madaniyat vazirligi</p>
       </a>
       <a href="#" class="grid-card-items">
-        <img src="../images/card1.png" alt="">
+        <div>
+          <img src="../images/card1.png" alt="">
+        </div>
         <p>Respublika Ma’naviyat va Ma’rifat markazi</p>
       </a>
       <a href="#" class="grid-card-items">
-        <img src="../images/card2.png" alt="">
+        <div>
+          <img src="../images/card2.png" alt="">
+        </div>
         <p>O’zbekiston Badiiy
           akademiya</p>
       </a>
       <a href="#" class="grid-card-items">
-        <img src="../images/card3.png" alt="">
+        <div>
+          <img src="../images/card3.png" alt="">
+        </div>
         <p>O’zbekiston Yozuvchilar
           uyushmasi</p>
       </a>
       <a href="#" class="grid-card-items">
-        <img src="../images/card4.png" alt="">
+        <div>
+          <img src="../images/card4.png" alt="">
+        </div>
         <p>O’zbekiston Islom 
           sivilizatsiya markazi</p>
       </a>
       <a href="#" class="grid-card-items">
-        <img src="../images/card5.png" alt="">
+        <div>
+          <img src="../images/card5.png" alt="">
+        </div>
         <p>O’zbekiston Milliy kutubxonasi</p>
       </a>
       <a href="#" class="grid-card-items">
-        <img src="../images/card6.png" alt="">
+        <div>
+          <img src="../images/card6.png" alt="">
+        </div>
         <p>O’zbekiston Fanlar
           akademiyasi</p>
       </a>

b41e70cbd9a58581a058784b2f7a06b196793401
Farrux <farruxzokirov230596@gmail.com>
2021-06-27 01:18:33 +0500

    section four add

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -659,6 +659,55 @@
   @include('components.scholars')
 
 
+  {{-- Section 4 Maqolalar --}}
+  <section class="article container mx-auto">
+    <div class="article-content-one">
+      <div class="article-content-one-head">
+        <div class="article-content-one-head-text flex items-center justify-between">
+          <h1 class="">Ilm-fan yangiliklari</h1>
+          <a href="#">Barchasi</a>
+        </div>
+        <div class="line-gradient-four"></div>
+        <div class="article-cards flex">
+          <a href="#" class="cards-item">
+            <img src="../images/article1.png" alt="">
+            <span class="bookmark3"><span class=" iconify  text-white" data-icon="mdi:bookmark-outline" data-inline="false"></span></span>
+                
+            <p class="text-white">Yangi O’zbekiston
+              ma’naviy asoslarini
+              mustahkamlashda ...”</p>
+          </a>
+          <a href="#" class="cards-item">
+            <img src="../images/article2.png" alt="">
+            <span class="bookmark3"><span class=" iconify  text-white" data-icon="mdi:bookmark-outline" data-inline="false"></span></span>
+                
+            <p class="text-white">Abdulla Oripov hayoti
+              va ijodi haqida</p>
+          </a>
+          <a href="#" class="cards-item">
+            <img src="../images/article3.png" alt="">
+            <span class="bookmark3"><span class=" iconify  text-white" data-icon="mdi:bookmark-outline" data-inline="false"></span></span>
+                
+            <p class="text-white">O’zbek milliy san’ati
+              ko’rgazmasi tashkil
+              etildi</p>
+          </a>
+          <a href="#" class="cards-item">
+            <img src="../images/article4.png" alt="">
+            <span class="bookmark3"><span class=" iconify  text-white" data-icon="mdi:bookmark-outline" data-inline="false"></span></span>
+                
+            <p class="text-white">Abdulla Qodiriy
+              kashfiyoti</p>
+          </a>
+        </div>
+
+      </div> 
+    </div>
+
+    <div class="article-content-two"></div>
+  </section>
+
+
 
   {{-- Section book slide --}}
   @include('components.bookslide')
